94eac69dff04c663f75e89aa5a6ece750f5e46f4
codewithneon <codewithneon@gmail.com>
2021-06-02 06:37:19 +0600

    init

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
